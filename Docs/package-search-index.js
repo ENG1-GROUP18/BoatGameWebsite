1fd3475c7a48c48e4a39bcac465253293bb92877
Morgan-Elkins <me839@york.ac.uk>
2022-05-04 02:32:39 +0100

    Added Upto date JavaDocs

--- Docs/package-search-index.js
+++ Docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.boatcorp.boatgame"},{"l":"com.boatcorp.boatgame.entities"},{"l":"com.boatcorp.boatgame.frameworks"},{"l":"com.boatcorp.boatgame.screens"},{"l":"com.boatcorp.boatgame.tools"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.boatcorp.boatgame"},{"l":"com.boatcorp.boatgame.entities"},{"l":"com.boatcorp.boatgame.frameworks"},{"l":"com.boatcorp.boatgame.screens"},{"l":"com.boatcorp.boatgame.tools"}]
